2ed281a63f2ad4f921901f4b129d6073995a4b14
MrTipson <gunfight7.gf@gmail.com>
2025-03-29 11:07:44 +0100

    Update masina monitor configuration

--- hosts/masina/home-manager/hyprland.nix
+++ hosts/masina/home-manager/hyprland.nix
@@ -3,7 +3,7 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [ # hyprctl monitors 
-        "DP-4, 2560x1440, 0x0, 1, vrr, 1"
+        "DP-4, 2560x1440@240, 0x0, 1, vrr, 2"
         "DVI-D-1, 1920x1080, 2560x0, 1, transform, 1"
         "Unknown-1, disable"
         ", preferred, auto, 1" # catch all for random monitors
