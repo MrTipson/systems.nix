b8ae061787341909452287a209773a07079ef0e3
MrTipson <mr@tipson.xyz>
2025-08-07 16:23:09 +0200

    Point nixpkgs in the system registry to a github ref

--- modules/registry.nix
+++ modules/registry.nix
@@ -3,7 +3,12 @@
 # This is useful so that a single nixpkgs instance can be shared by many flakes
 # For example, a standalone home-manager installation
 { inputs, pkgs, config, ... }: {
-  nix.registry."nixpkgs".flake = inputs.nixpkgs;
+  nix.registry."nixpkgs".to = {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = inputs.nixpkgs.rev;
+    type = "github";
+  };
   nix.package = pkgs.nix.overrideAttrs (old: {
     patches = old.patches ++ [ ./registry.patch ];
   });
