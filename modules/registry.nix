169f4b5caaae64e082ffbd13efece6c077819718
MrTipson <mr@tipson.xyz>
2025-10-31 22:19:19 +0100

    Update registry patching

--- modules/registry.nix
+++ modules/registry.nix
@@ -2,14 +2,18 @@
 # This module makes it so that flakes on this machine may reference registry entries in the lockfile
 # This is useful so that a single nixpkgs instance can be shared by many flakes
 # For example, a standalone home-manager installation
-{ inputs, pkgs, config, ... }: {
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}:
+{
   nix.registry."nixpkgs".to = {
     owner = "NixOS";
     repo = "nixpkgs";
     rev = inputs.nixpkgs.rev;
     type = "github";
   };
-  nix.package = pkgs.nix.overrideAttrs (old: {
-    patches = old.patches ++ [ ./registry.patch ];
-  });
+  nix.package = pkgs.nix.appendPatches [ ./registry.patch ];
 }
