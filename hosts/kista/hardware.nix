0cfb038daea3cb7224076058d1e12297844fd75b
MrTipson <mr@tipson.xyz>
2026-02-22 13:08:54 +0100

    kista: Update disk/samba configuration

--- hosts/kista/hardware.nix
+++ hosts/kista/hardware.nix
@@ -30,6 +30,10 @@
       device = "storage/user";
       fsType = "zfs";
     };
+    "/storage/zaklad" = {
+      device = "storage/zaklad";
+      fsType = "zfs";
+    };
     # sudo zfs create -o mountpoint=legacy storage/music
     # sudo zfs set sharenfs=on storage/music
     "/storage/music" = {
@@ -43,8 +47,9 @@
     description = "Samba users";
     group = "samba";
   };
-  users.groups.samba = {};
-  services.samba = { # sudo smbpasswd -a my_user
+  users.groups.samba = { };
+  services.samba = {
+    # sudo smbpasswd -a my_user
     enable = true;
     openFirewall = true;
     settings = {
@@ -57,10 +62,39 @@
         # "min protocol" = "smb3";
         #"max protocol" = "smb2";
         # note: localhost is the ipv6 localhost ::1
-        "hosts allow" = "192.168.64. 10.0.0. 127.0.0.1 localhost";
+        "hosts allow" = toString [
+          "192.168.64." # local network
+          "10.0.0." # lan
+          "100.64.0." # tailscale network
+          "127.0.0.1"
+          "localhost"
+        ];
         "hosts deny" = "0.0.0.0/0";
         "guest account" = "nobody";
         "map to guest" = "bad user";
+      }
+      # macos compatibility
+      # idk how much of this is actually needed but i cant be bothered to check
+      // {
+        "vfs objects" = "catia fruit streams_xattr";
+        "fruit:aapl" = "yes";
+        "fruit:nfs_aces" = "no";
+        "fruit:zero_file_id" = "yes";
+        "fruit:metadata" = "stream";
+        "fruit:encoding" = "native";
+        "spotlight backend" = "tracker";
+        "readdir_attr:aapl_rsize" = "no";
+        "readdir_attr:aapl_finder_info" = "no";
+        "readdir_attr:aapl_max_access" = "no";
+        "fruit:model" = "MacSamba";
+        "fruit:posix_rename" = "yes";
+        "fruit:veto_appledouble" = "no";
+        "fruit:wipe_intentionally_left_blank_rfork" = "yes";
+        "fruit:delete_empty_adfiles" = "yes";
+
+        # (OPTIONAL) Don't allow MacOS clients to write .DS_Store files to server shares
+        "veto files" = "/.snap/.sujournal/._.DS_Store/.DS_Store/.Trashes/.TemporaryItems/";
+        "delete veto files" = "yes";
       };
       "music" = {
         "path" = "/storage/music";
@@ -72,6 +106,17 @@
         "force user" = "samba";
         "force group" = "samba";
       };
+      "zaklad" = {
+        "path" = "/storage/zaklad";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0755";
+        "directory mask" = "0755";
+        "force user" = "samba";
+        "force group" = "samba";
+      };
+
     };
   };
 
