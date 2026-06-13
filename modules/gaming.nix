d59bfda30fad4e6ff39f53f821c4b07aacbdc709
MrTipson <mr@tipson.xyz>
2026-06-13 14:41:42 +0200

    Use protonup-ng

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -8,14 +8,13 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
-  
+
   environment.systemPackages = with pkgs; [
-    protonup
+    protonup-ng
   ];
 
   environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
-     "\${HOME}/.steam/root/compatibilitytools.d";
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
   };
 
   # then do this:
